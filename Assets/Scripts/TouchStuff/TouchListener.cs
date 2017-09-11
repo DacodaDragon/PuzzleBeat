0d949316bfcf62ad4f558bd47f690cd8d3da4889
Errorcodebin <errorcodebin_2@hotmail.com>
2017-09-11 18:29:00 +0200

    Wrapped Touch Input to Delegates

---
+++ Assets/Scripts/TouchStuff/TouchListener.cs
@@ -0,0 +1,59 @@
+﻿using Android.Touchmanager;
+using UnityEngine;
+
+public delegate void OnTouchPress();
+public delegate void OnTouchRelease();
+public delegate void OnTouchTap();
+public delegate void OnTouchMultiTap(int amount);
+
+public class TouchListener : MonoBehaviour
+{
+    private int m_TouchID;
+    private bool m_Touched = false;
+    private TouchManager manager;
+
+    public OnTouchPress onTouchPress;
+    public OnTouchRelease onTouchRelease;
+    public OnTouchTap onTouchTap;
+    public OnTouchMultiTap onTouchMultiTap;
+
+    void Start()
+    {
+        manager = FindObjectOfType<TouchManager>();
+    }
+
+    public Touch GetTouch()
+    {
+        if (m_Touched)
+            return Input.GetTouch(m_TouchID);
+        else return new Touch();
+    }
+
+    public void RecieveTouchID(int ID)
+    {
+        if (m_Touched)
+            return;
+        m_Touched = true;
+        m_TouchID = ID;
+        manager.onTouchLost += OnTouchLost;
+
+        if (onTouchPress != null)
+            onTouchPress();
+        if (onTouchTap != null)
+            onTouchTap();
+        if (onTouchMultiTap != null)
+            onTouchMultiTap(Input.GetTouch(ID).tapCount);
+    }
+
+    public void OnTouchLost(int ID)
+    {
+        if (ID == m_TouchID)
+        {
+            m_Touched = false;
+            manager.onTouchLost -= OnTouchLost;
+
+            if (onTouchRelease != null)
+                onTouchRelease();
+        }
+    }
+}
