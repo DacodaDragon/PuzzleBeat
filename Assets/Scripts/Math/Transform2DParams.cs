0355373c871c210c0680a2b8dd674a655cf3d82c
Errorcodebin <errorcodebin_2@hotmail.com>
2017-09-21 14:37:36 +0200

    Rotation fix

---
+++ Assets/Scripts/Math/Transform2DParams.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public struct Transform2DParams
+{
+    public Vector2 position;
+    public Quaternion rotation;
+
+    public Transform2DParams(Vector2 position, Quaternion rotation)
+    {
+        this.position = position;
+        this.rotation = rotation;
+    }
+}
