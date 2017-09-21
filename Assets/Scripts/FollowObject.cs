a1748bb4b93eec512930eb2ddb0c18441042b084
Errorcodebin <errorcodebin_2@hotmail.com>
2017-09-21 14:38:34 +0200

    Added FollowObject

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_ObjectToFollow;
+
+    public void LateUpdate()
+    {
+        transform.position = m_ObjectToFollow.transform.position + new Vector3(0, 0, -10);
+    }
+}
