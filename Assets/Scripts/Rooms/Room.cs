0b21be3b5258a9520d61524e12390a4f9ecd092b
Errorcodebin <errorcodebin_2@hotmail.com>
2017-09-22 10:36:32 +0200

    Created incomplete function for listening to puzzle elements

--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -6,20 +6,23 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     [SerializeField] private string m_IDString;
-    [SerializeField] private int    m_IDNumber; 
-    [SerializeField] private int    m_LengthInBeats;
+    [SerializeField] private int m_IDNumber;
+    [SerializeField] private int m_LengthInBeats;
 
     private RoomPath m_roomPath;
+    private Gate m_gate;
 
-    public int ID               { get { return m_IDNumber; } }
-    public int BeatLength       { get { return m_LengthInBeats; } set { m_LengthInBeats = value; } }
-    public RoomPath Path        { get { return m_roomPath; } }
-    public PathNode EndNode     { get { return m_roomPath.EndNode; } }
-    public PathNode StartNode   { get { return m_roomPath.StartNode; } }
+    public int ID { get { return m_IDNumber; } }
+    public int BeatLength { get { return m_LengthInBeats; } set { m_LengthInBeats = value; } }
+    public RoomPath Path { get { return m_roomPath; } }
+    public PathNode EndNode { get { return m_roomPath.EndNode; } }
+    public PathNode StartNode { get { return m_roomPath.StartNode; } }
 
     void Awake()
     {
         m_roomPath = GetComponent<RoomPath>();
+        m_gate = GetComponentInChildren<Gate>();
+        HookPuzzleElements(GetComponentsInChildren<PuzzleElement>(true));
     }
 
     public void Enable()
@@ -49,5 +52,10 @@ public class Room : MonoBehaviour
         Vector2 position = Vector2.Lerp(s[segment], s[segment + 1], DistanceToTravelInSegment / SegmentLength);
         Quaternion rotation = Quaternion.Euler(0, 0, Vector2DMath.GetAngleBetween(s[segment], s[segment + 1]));
         return new Transform2DParams(position, rotation);
+    }
+
+    private void HookPuzzleElements(PuzzleElement[] elements)
+    {
+        // [TODO] Hook Puzzle elements to gate?
     }
 }
