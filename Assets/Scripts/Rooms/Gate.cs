8cba02d9f9c5ac8aa8ad65440f9d7343a7639dea
Errorcodebin <errorcodebin_2@hotmail.com>
2017-09-22 10:37:18 +0200

    Added Gate PuzzleElement
    I forgot to push this one along with the one previously

---
+++ Assets/Scripts/Rooms/Gate.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using DDR;
+
+public class Gate : PuzzleElement
+{
+    private int MaxSolveAmount = 0;
+    private int CurrentSolveAmount = 0;
+
+    public void Start()
+    {
+        OnSolveEvent += Open;
+        FindObjectOfType<MusicPlayer>().OnWhole += Bounce;
+    }
+
+    private void Bounce()
+    {
+        // Bounce (for animation purposes)
+    }
+
+    // Its kind of ugle because we hook it in the
+    // puzzleelements's onSolve event.. I'll patch
+    // this up some other time, right now it just
+    // needs to work.
+    private void Open(PuzzleElement element)
+    {
+        // [TODO] Open Gate
+        // Open Gate Animation
+        // Disable life sequence thing
+    }
+
+    public void SolveReciever(PuzzleElement element)
+    {
+        CurrentSolveAmount += 1;
+        if (CurrentSolveAmount == MaxSolveAmount)
+            Solve();
+    }
+}
