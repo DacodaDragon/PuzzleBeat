5c5d054f200e3d462a21c0ca41f4980234e980bb
Errorcodebin <errorcodebin_2@hotmail.com>
2017-09-22 19:00:32 +0200

    Created BeatObserver
    The goal of beat observers are to invoke delegates at measures of the song

---
+++ Assets/Scripts/Timing/BeatObserver.cs
@@ -0,0 +1,46 @@
+﻿namespace DDR
+{
+    public class BeatObserver
+    {
+        private OnBeatDelegate m_OnTrigger;
+        private float m_nextMeasure;
+        private float m_previousMeasure;
+        private float m_measure;
+
+        public event OnBeatDelegate OnTrigger { add { m_OnTrigger += value; } remove { m_OnTrigger -= value; }}
+
+        public float Measure { get { return m_measure; } }
+
+        public BeatObserver(float beatMeasure, float currentTime = 0)
+        {
+            m_measure         = beatMeasure;
+            m_nextMeasure     = currentTime + beatMeasure;
+            m_previousMeasure = currentTime - beatMeasure;
+        }
+
+        public void Update(float timeInBeats)
+        {
+            // Time forwards!
+            if (timeInBeats > m_nextMeasure)
+            {
+                m_nextMeasure     += m_measure;
+                m_previousMeasure += m_measure;
+                Trigger();
+            }
+
+            // Time backwards!
+            if (timeInBeats < m_previousMeasure)
+            {
+                m_nextMeasure     += m_measure;
+                m_previousMeasure += m_measure;
+                Trigger();
+            }
+        }
+
+        private void Trigger()
+        {
+            if (m_OnTrigger != null)
+                m_OnTrigger.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Timing/BeatObserverManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DDR
+{
+    public class BeatObserverManager
+    {
+        private List<BeatObserver> m_Observers = new List<BeatObserver>();
+
+        public void SubScribe(OnBeatDelegate function, float beatMeasure)
+        {
+            // Search for active BeatObserver
+            for (int i = 0; i < m_Observers.Count; i++)
+            {
+                if (m_Observers[i].Measure == beatMeasure)
+                {
+                    m_Observers[i].OnTrigger += function;
+                    return; 
+                }
+            }
+
+            // Add new BeatObserver if there is none for the measure yet. 
+            BeatObserver observer = new BeatObserver(beatMeasure);
+            observer.OnTrigger += function;
+            m_Observers.Add(observer);
+        }
+
+        public void UnSubscribe(OnBeatDelegate function, float beatMeasure)
+        {
+            for (int i = 0; i < m_Observers.Count; i++)
+            {
+                if (m_Observers[i].Measure == beatMeasure)
+                {
+                    m_Observers[i].OnTrigger -= function;
+                    return;
+                }
+            }
+        }
+
+        public void Update(float timeInBeats)
+        {
+            for (int i = 0; i < m_Observers.Count; i++)
+            {
+                m_Observers[i].Update(timeInBeats);
+            }
+        }
+
+        public void RemoveAllObservers()
+        {
+            m_Observers.Clear();
+        }
+    }
+}
